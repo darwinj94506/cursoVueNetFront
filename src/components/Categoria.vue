4ed537cd94f42bc64a20f062e429f1d81f1bee0d
darwinj94 <darwinj94@hotmail.com>
2020-01-19 22:44:53 -0500

    todo bien hasta aquí

---
+++ src/components/Categoria.vue
@@ -0,0 +1,28 @@
+<template>
+ <h2>categorias</h2>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                        
+            }
+        },
+        computed:{
+            
+        },
+        watch:{
+
+        },
+        created(){
+
+        },
+        methods:{
+            initialize(){
+                
+            }
+
+        }
+
+    }
+</script>
